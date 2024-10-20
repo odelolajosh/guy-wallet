a39f92dc38574783d040189dd8f03655f5416bf2
Joshua Odelola <oladapoodelola@gmail.com>
2024-10-20 15:02:03 +0100

    refactor: Update server configuration and dependencies

--- guypay-ts/http/server.ts
+++ guypay-ts/http/server.ts
@@ -1,11 +1,15 @@
-import express from "express";
-import { handleError } from "./middlewares/error";
-import { createRoutes } from "./routes";
+import express from "express"
+import morgan from "morgan"
+import cors from "cors"
+import { handleError } from "./middlewares/error"
+import { createRoutes } from "./routes"
 
-export const app = express();
+export const app = express()
 
 app.use(express.json())
+app.use(cors())
+app.use(morgan("tiny"))
 
-app.use("/v0", createRoutes())
+app.use("/api/v0", createRoutes())
 
 app.use(handleError)
