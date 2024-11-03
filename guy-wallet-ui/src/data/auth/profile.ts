d6d90ed074b068bf94e2c7d89b3fd952b4887197
Joshua Odelola <oladapoodelola@gmail.com>
2024-11-03 21:22:31 +0100

    refactor: Update Table component styles for improved layout and remove unused user data example

--- guy-wallet-ui/src/data/auth/profile.ts
+++ guy-wallet-ui/src/data/auth/profile.ts
@@ -7,20 +7,6 @@ export interface User {
   createdAt: string;
 }
 
-/*
-{
-    "id": 3,
-    "user": {
-        "id": "97e9b54a-5405-49f4-a4b9-72d3b0d66644",
-        "uid": "NAysndQMkYWS8ooTOikj9x9UzUO2",
-        "email": "testmail4@mail.com",
-        "first_name": "test1",
-        "last_name": "last1"
-    },
-    "picture": null
-}
-    */
-
 export interface UserResponse {
   user: {
     id: string
