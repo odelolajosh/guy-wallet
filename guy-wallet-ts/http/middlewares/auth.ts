a4f1d990fa56161251d88a5830e77d8e60c2ae26
Joshua Odelola <oladapoodelola@gmail.com>
2024-11-04 02:48:18 +0100

    feat: Enhance authorization middleware with error handling for access token verification

--- guy-wallet-ts/http/middlewares/auth.ts
+++ guy-wallet-ts/http/middlewares/auth.ts
@@ -1,3 +1,4 @@
+import { InvalidAccessTokenError } from "@/application/auth/auth-error"
 import { IAuthService } from "@/application/auth/auth-interface"
 import { NextFunction, Request, Response } from "express"
 
@@ -7,26 +8,25 @@ import { NextFunction, Request, Response } from "express"
  */
 export const requireAuth = (authService: IAuthService) => (
   async (request: Request, response: Response, next: NextFunction) => {
-    const authorization = request.headers["authorization"]
+    try {
+      const authorization = request.headers["authorization"]
 
-    if (!authorization) {
-      response.status(401).json({ message: "Authorization header missing." })
-      return
+      if (!authorization) {
+        response.status(401).json({ message: "Authorization header missing." })
+        return
+      }
+
+      const [strategy, token] = authorization.split(" ")
+      if (strategy !== "Bearer" || !token) {
+        response.status(401).json({ message: "Invalid authorization format. Token missing." })
+        return
+      }
+
+      const user = await authService.verifyAccessToken(token)
+      request.user = user;
+      next()
+    } catch (error) {
+      next(error)
     }
-
-    const [strategy, token] = authorization.split(" ")
-    if (strategy !== "Bearer" || !token) {
-      response.status(401).json({ message: "Invalid authorization format. Token missing." })
-      return
-    }
-
-    const user = await authService.verifyAccessToken(token)
-    if (!user) {
-      response.status(403).json({ message: "Invalid or expired token" });
-      return
-    }
-
-    request.user = user;
-    next()
   }
 )
