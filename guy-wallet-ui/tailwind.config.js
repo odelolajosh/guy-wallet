0a69bbbec3591f56cfc5c7a5648d61237c20fadc
Joshua Odelola <oladapoodelola@gmail.com>
2024-11-06 11:48:49 +0100

    feat: Update Tailwind CSS configuration to extend width and clean up unused utilities

--- guy-wallet-ui/tailwind.config.js
+++ guy-wallet-ui/tailwind.config.js
@@ -1,11 +1,3 @@
-
-const svgToDataUri = require("mini-svg-data-uri");
-
-const colors = require("tailwindcss/colors");
-const {
-	default: flattenColorPalette,
-} = require("tailwindcss/lib/util/flattenColorPalette");
-
 /** @type {import('tailwindcss').Config} */
 export default {
 	darkMode: ["class"],
@@ -15,6 +7,9 @@ export default {
 	],
 	theme: {
     	extend: {
+				width: {
+					128: "32rem",
+				},
     		borderRadius: {
     			lg: 'var(--radius)',
     			md: 'calc(var(--radius) - 2px)',
@@ -50,10 +45,6 @@ export default {
     			accent: {
     				DEFAULT: 'hsl(var(--accent))',
     				foreground: 'hsl(var(--accent-foreground))'
-    			},
-					neutral: {
-    				DEFAULT: 'hsl(var(--neutral))',
-    				foreground: 'hsl(var(--neutral-foreground))'
     			},
     			destructive: {
     				DEFAULT: 'hsl(var(--destructive))',
@@ -91,35 +82,5 @@ export default {
     },
 	plugins: [
 		require("tailwindcss-animate"),
-		addVariablesForColors,
-		function ({ matchUtilities, theme }: any) {
-			matchUtilities(
-				{
-					"bg-grid": (value: any) => ({
-						backgroundImage: `url("${svgToDataUri(
-							`<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="32" height="32" fill="none" stroke="${value}"><path d="M0 .5H31.5V32"/></svg>`
-						)}")`,
-					}),
-					"bg-grid-small": (value: any) => ({
-						backgroundImage: `url("${svgToDataUri(
-							`<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="8" height="8" fill="none" stroke="${value}"><path d="M0 .5H31.5V32"/></svg>`
-						)}")`,
-					}),
-					"bg-dot": (value: any) => ({
-						backgroundImage: `url("${svgToDataUri(
-							`<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="16" height="16" fill="none"><circle fill="${value}" id="pattern-circle" cx="10" cy="10" r="1.6257413380501518"></circle></svg>`
-						)}")`,
-					}),
-				},
-				{ values: flattenColorPalette(theme("backgroundColor")), type: "color" }
-			);
-		},
 	],
-}
-
-function addVariablesForColors({ addBase, theme }: any) {
-	let allColors = flattenColorPalette(theme("colors"));
-	let newVars = Object.fromEntries(
-		Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
-	);
 }
